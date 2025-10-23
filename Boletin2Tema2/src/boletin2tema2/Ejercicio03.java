657b674861416d89ffd6f9c9ba55f355dcf0cd25
Jade <jade.belloso@iesnervion.es>
2025-10-23 13:21:44 +0200

    HECHO SEGUNDO BOLETIN OLE Q OLEEEEEE

--- Boletin2Tema2/src/boletin2tema2/Ejercicio03.java
+++ Boletin2Tema2/src/boletin2tema2/Ejercicio03.java
@@ -6,50 +6,59 @@ public class Ejercicio03 {
 
 	public static void main(String[] args) {
 
-		//Create the scanner
+		// Create the scanner
 		Scanner sc = new Scanner(System.in);
 
 		// Create variable for the numbers and the operation choosen by the user
 		int num1;
 		int num2;
-		int eleccion;
-		
+		String eleccion;
+
 		// Ask the user for the numbers
 		System.out.println("Introduce dos números:");
 		// And read them
 		num1 = sc.nextInt();
 		num2 = sc.nextInt();
-		
-		//Now we make the user choose an operation for both of these numbers
-		System.out.println("Elije una de las siguientes operaciones para hacer con los números introducidos: " + num1 +" y " + num2);
-		System.out.println("1) SUMAR LOS NÚMEROS\r\n"
-				+ "2) RESTAR LOS NÚMEROS\r\n"
-				+ "3) MULTIPLICAR LOS NÚMEROS\r\n"
-				+ "4) DIVIDIR LOS NÚMEROS");
-		
-		//Read the user election
-		eleccion = sc.nextInt();
-		
-		//Now make a switch for each operation the user can choose to do with their numbers.
-		switch(eleccion){
-		case 1:
-			System.out.println("La suma de ambos números es: " + (num1+num2));
+
+		// Now we make the user choose an operation for both of these numbers
+		System.out.println("Elije una de las siguientes operaciones para hacer con los números introducidos: " + num1
+				+ " y " + num2);
+		System.out.println("A) SUMAR LOS NÚMEROS\r\n" + "B) RESTAR LOS NÚMEROS\r\n" + "C) MULTIPLICAR LOS NÚMEROS\r\n"
+				+ "D) DIVIDIR LOS NÚMEROS");
+
+		// Read the user election
+		eleccion = sc.next();
+
+		// Now make a switch for each operation the user can choose to do with their
+		// numbers.
+		switch (eleccion) {
+		case "A":
+			System.out.println("La suma de ambos números es: " + (num1 + num2));
 			break;
-		case 2:
+//Case for substracting the numbers
+		case "B":
 			System.out.println("La resta de ambos números es: " + (num1 - num2));
 			break;
-		case 3:
-			System.out.println("La multiplicación de ambos números es: " + (num1*num2));
+		//Case for the product
+		case "C":
+			System.out.println("La multiplicación de ambos números es: " + (num1 * num2));
 			break;
-		case 4:
-			System.out.println("La división de ambos números es: " + (num1/num2));
+		// In this case we also make a condition bc if the second number is 0 then we
+		// cant divide, so it gives a error message
+		case "D": {
+			if (num2 == 0) {
+				System.out.println("ERROR. No se puede dividir entre 0");
+			} else {
+				System.out.println("La división de ambos números es: " + (num1 / num2));
+			}
 			break;
-		default: 
+		}
+
+		default:
 			System.out.println("ERROR. La opción seleccionada no es ninguna de las operaciones posibles");
-	}
-		//And close the scanner
+		}
+		// And close the scanner
 		sc.close();
 
-		}
 	}
-
+}
--- Boletin2Tema2/src/boletin2tema2/Ejercicio04.java
+++ Boletin2Tema2/src/boletin2tema2/Ejercicio04.java
@@ -8,9 +8,14 @@ public class Ejercicio04 {
 
 		Scanner sc = new Scanner(System.in);
 
-		// Create variable for the grade
+		// Create variable for each dice roll
 		String tirada1;
 		String tirada2;
+		
+		// Create new variables for the numbers
+
+		int tiradaN1 = 0;
+		int tiradaN2 = 0;
 		
 		// Ask the user for the result of the first dice
 		System.out.println("Introduce el resultado del primer dado (Ej: DOS): ");
@@ -20,23 +25,62 @@ public class Ejercicio04 {
 		System.out.println("Y ahora el resultado del segundo");
 		tirada2 = sc.next();
 		
-		//Now make a switch for each result the user can have on their dices
+		//Now make a switch for each result the user can have on their dices on the first dice roll
 		switch(tirada1){
-		case UNO:
-			
-			break;
-		case 2:
-			System.out.println("La resta de ambos números es: " + (num1 - num2));
-			break;
-		case 3:
-			System.out.println("La multiplicación de ambos números es: " + (num1*num2));
-			break;
-		case 4:
-			System.out.println("La división de ambos números es: " + (num1/num2));
-			break;
-		default: 
-			System.out.println("ERROR. La opción seleccionada no es ninguna de las operaciones posibles");
+		case "UNO"-> {
+			tiradaN1 = 1;
+		}
+		case "DOS"-> {
+			tiradaN1 = 2;
+		}
+		case "TRES"-> {
+			tiradaN1 = 3;
+		}
+		case "CUATRO"-> {
+			tiradaN1 = 4;
+		}
+		case "CINCO"-> {
+			tiradaN1 = 5;
+		}
+		case "SEIS"-> {
+			tiradaN1 = 6;
+
+		}
+
+		default -> 
+			System.out.println("ERROR. ¡Un dado solo puede sacar números hasta 6!");
 	}
+		
+		//Then for the second dice roll
+				switch(tirada2){
+				case "UNO"-> {
+					tiradaN2 = 1;
+				}
+				case "DOS"-> {
+					tiradaN2 = 2;
+				}
+				case "TRES"-> {
+					tiradaN2 = 3;
+				}
+				case "CUATRO"-> {
+					tiradaN2 = 4;
+				}
+				case "CINCO"-> {
+					tiradaN2 = 5;
+				}
+				case "SEIS"-> {
+					tiradaN2 = 6;
+
+				}
+
+				default -> 
+					System.out.println("ERROR. ¡El dado solo puede sacar números hasta 6!");
+			}
+				
+				//Finally, make the operation and print the sum of the 2 dice rolls
+			
+				System.out.println("Tu tirada resulta en " + (tiradaN1 + tiradaN2));
+				
 		//And close the scanner
 		sc.close();
 
