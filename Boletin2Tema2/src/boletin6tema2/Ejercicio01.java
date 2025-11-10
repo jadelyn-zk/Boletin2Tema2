b89699d6f434b352804871f1981226b38a6c7d90
Jade <jade.belloso@iesnervion.es>
2025-11-10 08:58:29 +0100

    a

---
+++ Boletin2Tema2/src/boletin6tema2/Ejercicio01.java
@@ -0,0 +1,16 @@
+package boletin6tema2;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		//create scanner
+		Scanner sc=new Scanner(System.in);
+		
+		
+		//close the scanner
+		sc.close();
+	}
+
+}
